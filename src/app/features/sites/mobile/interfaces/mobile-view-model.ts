ef924ec0c0414cc129baef103f2573cbb5744c6f
Maria-Ezzat <m.wanis@kog-eg.com>
2025-04-12 15:16:41 +0200

    Make a get All mobile

--- src/app/features/sites/mobile/interfaces/mobile-view-model.ts
+++ src/app/features/sites/mobile/interfaces/mobile-view-model.ts
@@ -1,2 +1,33 @@
-export interface MobileViewModel {
+export interface mobileViewModel {
+  id: string;
+  username:string
+  mobileModel: string;
+  IMEI1: string;
+  phoneStatus: string;
+  IMEI2: string;
+  selected?: boolean;
 }
+
+export class mobileSearchViewModel {
+  SearchText: string;
+  PhoneStatus: number;
+  
+}
+export class mobileSelectedViewModel {
+  id: string;
+  name: string;
+}
+export class mobileActivateViewModel {
+  id: string;
+}
+export class mobileCreateViewModel {
+  id: string;
+  name: string;
+  nationalNumber?: string;
+  password: string;
+  mobile: string;
+  email?: string;
+  confirmPassword: string;
+  paths?: string[];
+}
+
--- src/app/features/sites/mobile/mobile.module.ts
+++ src/app/features/sites/mobile/mobile.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './components/home/home.component';
 import { CreateComponent } from './components/create/create.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from 'primeng/api';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { TableModule } from 'primeng/table';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
   { path: '', component:HomeComponent }, 
-  { path:'create', component: CreateComponent },
+  { path:'create/:id', component: CreateComponent },
   { path:'edit/:id', component: CreateComponent },
 
 
