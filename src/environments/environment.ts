3baa2363e828b62d80495e2de23b5df5eed57a5b
Maria-Ezzat <125491036+MariaEzat@users.noreply.github.com>
2025-04-27 13:10:47 +0300

    Change environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   pageSize: 50,
 //  api: `https://20.164.22.129`,
  //api:`http://www.ecommerce-api.com`
- api:`https://20.164.21.172`
+ api:`https://api.rage3ly.com`
 };
