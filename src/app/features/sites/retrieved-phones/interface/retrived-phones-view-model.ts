f3659962ce5e68519768c367391d47aea3425221
Maria-Ezzat <125491036+MariaEzat@users.noreply.github.com>
2025-05-10 15:22:50 +0300

    finish RetrivedPhone Module

--- src/app/features/sites/retrieved-phones/interface/retrived-phones-view-model.ts
+++ src/app/features/sites/retrieved-phones/interface/retrived-phones-view-model.ts
@@ -1,2 +1,12 @@
 export interface RetrivedPhonesViewModel {
+  id:string;
+  clientName:string;
+  clientId:string;
+  mobileModel:string;
+  brandName:string;
 }
+
+export class searchRetrivedPhonesViewModel {
+  From:Date;
+  To:Date;
+  }
